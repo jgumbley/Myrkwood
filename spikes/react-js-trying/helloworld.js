ba25d7474286e33a15f7079bed6a0a269ec7118c
Jim Gumbley <jgumbley@thoughtworks.com>
2014-11-24 21:14:16 +0000

    doing the tutorial on reactjs

---
+++ spikes/react-js-trying/helloworld.js
@@ -0,0 +1,62 @@
+var data = [
+  {author: "George Bush", text: "Yo blair"},
+  {author: "Jordan Walke", text: "Hey George"}
+];
+
+var Comment = React.createClass({
+  render: function() {
+    return (
+      <div className="comment">
+        <h2 className="commentAuthor">
+          {this.props.author}
+        </h2>
+        {this.props.children}
+      </div>
+    );
+  }
+});
+
+var CommentList = React.createClass({
+  render: function() {
+    var commentNodes = this.props.data.map(function (comment) {
+      return (
+        <Comment author={comment.author}>
+          {comment.text}
+        </Comment>
+      );
+    });
+    return (
+      <div className="commentList">
+        {commentNodes}
+      </div>
+    )
+  }
+});
+
+var CommentForm = React.createClass({
+  render: function() {
+    return (
+      <div className="commentForm">
+        Hello, world! I am a CommentForm.
+      </div>
+    );
+  }
+});
+
+var CommentBox = React.createClass({
+  render: function() {
+    return (
+      <div className="commentBox">
+          <h1>Comments</h1>
+          <CommentForm />
+          <CommentList data={this.props.data} />
+      </div>
+    );
+  }
+});
+
+
+React.render(
+  <CommentBox data={data} />,
+  document.getElementById('example')
+);
