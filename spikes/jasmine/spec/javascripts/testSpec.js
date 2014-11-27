7a5cfba84ce8c6fc3cf4115c00f0769adb46b934
Jim Gumbley <jgumbley@thoughtworks.com>
2014-11-27 20:34:29 +0000

    silly test

--- spikes/jasmine/spec/javascripts/testSpec.js
+++ spikes/jasmine/spec/javascripts/testSpec.js
@@ -1,5 +1,5 @@
-describe("A suite", function() {
-  it("contains spec with an expectation", function() {
-    expect(true).toBe(true);
+describe("Law of excluded middle", function() {
+  it("implies that necessity holds", function() {
+    expect(true).toBe(false);
   });
 });
