d0eeb75070cf79959bf1ac4f40ea40c59407703a
Jim Gumbley <jgumbley@thoughtworks.com>
2014-11-30 18:12:23 +0000

    passing the test by dereferencing the dom object

--- spikes/jasmine/js/reactive.js
+++ spikes/jasmine/js/reactive.js
@@ -2,6 +2,8 @@ var wireUpSliderToLabel;
 
 wireUpSliderToLabel = function($slider, $label) {
   var sliderValue;
-  sliderValue = $slider.asEventStream('input');
+  sliderValue = $slider.asEventStream('input').map(function(event) {
+    return event.target.value;
+  });
   return sliderValue.assign($label, "text");
 };
