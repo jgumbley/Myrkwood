c690a02cfd17d853beb041277e86f8c91fa51090
Jim Gumbley <jgumbley@thoughtworks.com>
2014-12-12 20:58:24 +0000

    i learned how js might do a tree

--- spikes/jasmine/js_tests/tableSpec.js
+++ spikes/jasmine/js_tests/tableSpec.js
@@ -7,4 +7,16 @@ describe("Table of threats", function() {
     // then
     expect(threats[0].name).toBe("steal money");
   });
+  it("can make a simple tree", function() {
+    // given
+    tree = {branch1: 1, 
+            branch2: [{twig1: 2}, {twig2: 3}]
+            }
+    // when
+    // then
+    expect(tree.branch1).toBe(1);
+    expect(tree.branch2[0].twig1).toBe(2);
+    expect(tree.branch2[1].twig2).toBe(3);
+
+  });
 });
